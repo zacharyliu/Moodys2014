9de061d9e1c85814a8aa873842871567950a8d20
Zachary Liu <liu.zachary@gmail.com>
2014-03-09 14:26:25 -0400

    Class Model2

--- model2.py
+++ model2.py
@@ -4,74 +4,85 @@ import numpy as np
 import scipy.optimize
 import csv
 
-maxRepeats = 2
-p = [0.569, 0.403, 0.736, 0.736]
+class Model2:
+    def __init__(self,
+                 maxRepeats=2,
+                 P_vegetables=0.569,
+                 P_fruits=0.403,
+                 P_proteins=0.736,
+                 P_grains=0.736,
+                 USDA_fruits=2.5,
+                 USDA_vegetables=3.75,
+                 USDA_proteins=9.5,
+                 USDA_grains=9):
+        self.maxRepeats = maxRepeats
+        self.P = [P_vegetables, P_fruits, P_proteins, P_grains]
+        self.usda = {'fruits': USDA_fruits, 'vegetables': USDA_vegetables, 'proteins': USDA_proteins, 'grains': USDA_grains}
 
-with open('test2.csv', 'rb') as csvfile:
-    reader = csv.reader(csvfile)
-    columns = reader.next()[1:]
-    columnsLookup = {columns[i]: i for i in xrange(len(columns))}
-    dishes = list(reader)
-    dishesValues = np.array([dish[1:] for dish in dishes])
-    dishesValues = dishesValues.astype(float)
+        with open('test2.csv', 'rb') as csvfile:
+            reader = csv.reader(csvfile)
+            self.columns = reader.next()[1:]
+            self.columnsLookup = {self.columns[i]: i for i in xrange(len(self.columns))}
+            self.dishes = list(reader)
+            self.dishesValues = np.array([dish[1:] for dish in self.dishes])
+            self.dishesValues = self.dishesValues.astype(float)
 
-def scale(x):
-    return [np.multiply(x[i], dishesValues[i]) for i in xrange(len(x))]
+    def scale(self, x):
+        return [np.multiply(x[i], self.dishesValues[i]) for i in xrange(len(x))]
 
-def getAttr(x, attr):
-    return [dish[int(columnsLookup[attr])] for dish in scale(x)]
+    def getAttr(self, x, attr):
+        return [dish[int(self.columnsLookup[attr])] for dish in self.scale(x)]
 
-usda = {'fruits': 2.5, 'vegetables': 3.75, 'proteins': 9.5, 'grains': 9}
+    def constrainAttr(self, x, attr, value, isMaximum):
+        constraint = np.sum(self.getAttr(x, attr)) - value
+        if isMaximum:
+            constraint *= -1
+        return constraint
 
-def constrainAttr(x, attr, value, isMaximum):
-    constraint = np.sum(getAttr(x, attr)) - value
-    if isMaximum:
-        constraint *= -1
-    return constraint
+    def solve(self):
+        result = scipy.optimize.minimize(fun = self.constrainAttr,
+                                         # x0 = [1.0/len(dishes)]*len(dishes),
+                                         x0 = [random.random() * self.maxRepeats for i in xrange(len(self.dishes))],
+                                         args = ['calories', 0, True],
+                                         method='SLSQP',
+                                         bounds = [(0, self.maxRepeats)]*len(self.dishes),
+                                         constraints = [
+                                             {'type': 'ineq', 'fun': self.constrainAttr, 'args': ['price', 6, True]},
+                                             {'type': 'ineq', 'fun': self.constrainAttr, 'args': ['fruits', self.usda['fruits'], False]},
+                                             {'type': 'ineq', 'fun': self.constrainAttr, 'args': ['vegetables', self.usda['vegetables'], False]},
+                                             {'type': 'ineq', 'fun': self.constrainAttr, 'args': ['proteins', self.usda['proteins'], False]},
+                                             {'type': 'ineq', 'fun': self.constrainAttr, 'args': ['grains', self.usda['grains'], False]},
+                                         ],
+                                         options = {
+                                             'disp': True,
+                                         })
 
-result = scipy.optimize.minimize(fun = constrainAttr,
-                                 # x0 = [1.0/len(dishes)]*len(dishes),
-                                 x0 = [random.random() * maxRepeats for i in xrange(len(dishes))],
-                                 args = ['calories', 0, True],
-                                 method='SLSQP',
-                                 bounds = [(0, maxRepeats)]*len(dishes),
-                                 constraints = [
-                                     {'type': 'ineq', 'fun': constrainAttr, 'args': ['price', 6, True]},
-                                     {'type': 'ineq', 'fun': constrainAttr, 'args': ['fruits', usda['fruits'], False]},
-                                     {'type': 'ineq', 'fun': constrainAttr, 'args': ['vegetables', usda['vegetables'], False]},
-                                     {'type': 'ineq', 'fun': constrainAttr, 'args': ['proteins', usda['proteins'], False]},
-                                     {'type': 'ineq', 'fun': constrainAttr, 'args': ['grains', usda['grains'], False]},
-                                 ],
-                                 options = {
-                                     'disp': True,
-                                 })
+        output = np.sum(self.scale(result.x), axis=0)
+        print
+        print 'Output'
+        for i in xrange(len(output)):
+            print self.columns[i], ':', output[i]
 
-output = np.sum(scale(result.x), axis=0)
-print
-print 'Output'
-for i in xrange(len(output)):
-    print columns[i], ':', output[i]
+        print
+        print 'Number of each dish:'
+        for i in xrange(len(result.x)):
+            print self.dishes[i][0], np.round(result.x[i], 1)
 
-print
-print 'Number of each dish:'
-for i in xrange(len(result.x)):
-    print dishes[i][0], np.round(result.x[i], 1)
+        def modeledP(dishValues):
+            sugar = dishValues[int(self.columnsLookup['sugar'])]
+            sodium = dishValues[int(self.columnsLookup['sodium'])]
+            calcium = dishValues[int(self.columnsLookup['calcium'])]
+            A = 0.0445
+            B = 0.00513
+            C = -0.018
 
-def modeledP(dishValues):
-    sugar = dishValues[int(columnsLookup['sugar'])]
-    sodium = dishValues[int(columnsLookup['sodium'])]
-    calcium = dishValues[int(columnsLookup['calcium'])]
-    A = 0.0445
-    B = 0.00513
-    C = -0.018
+            P = 1-math.e**-(A*sugar + B*sodium + C*calcium)
+            return P
 
-    P = 1-math.e**-(A*sugar + B*sodium + C*calcium)
-    return P
+        def actualCalories(x):
+            return np.sum([self.dishesValues[i][int(self.columnsLookup['calories'])] * modeledP(self.dishesValues[i]) * x[i] for i in xrange(len(self.dishesValues))])
 
-def actualCalories(x):
-    return np.sum([dishesValues[i][int(columnsLookup['calories'])] * modeledP(dishesValues[i]) * x[i] for i in xrange(len(dishesValues))])
-
-print
-actual_calories = actualCalories(result.x)
-print 'Actual calorie consumption:', actual_calories
-print '=', actual_calories/5.0, 'per day'
+        print
+        actual_calories = actualCalories(result.x)
+        print 'Actual calorie consumption:', actual_calories
+        print '=', actual_calories/5.0, 'per day'
---
+++ run.py
@@ -0,0 +1,4 @@
+from model2 import Model2
+
+model = Model2()
+model.solve()
