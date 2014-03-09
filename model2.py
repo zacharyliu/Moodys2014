57529becfaeb0a2a6cd6c4b2b4164ab072154cd7
Zachary Liu <liu.zachary@gmail.com>
2014-03-09 13:26:14 -0400

    Initial commit.

---
+++ model2.py
@@ -0,0 +1,82 @@
+import math
+import random
+import numpy as np
+import scipy.optimize
+import csv
+
+maxRepeats = 2
+p = [0.569, 0.403, 0.736, 0.736]
+
+with open('realPrices.csv', 'rb') as csvfile:
+    reader = csv.reader(csvfile)
+    columns = reader.next()[1:]
+    columnsLookup = {columns[i]: i for i in xrange(len(columns))}
+    dishes = list(reader)
+    dishesValues = np.array([dish[1:] for dish in dishes])
+    dishesValues = dishesValues.astype(float)
+
+def scale(x):
+    return [np.multiply(x[i], dishesValues[i]) for i in xrange(len(x))]
+
+def getAttr(x, attr):
+    return [dish[int(columnsLookup[attr])] for dish in scale(x)]
+
+def calories(x):
+    n = sum(getAttr(x, 'calories'))
+    return -n
+
+def price(x):
+    total = sum(getAttr(x, 'price'))
+    return 6-total
+
+usda = {'fruits': 2.5, 'vegetables': 3.75, 'proteins': 9.5, 'grains': 9}
+
+def usda_fruits(x):
+    return np.sum(getAttr(x, 'fruits')) - usda['fruits']
+def usda_vegetables(x):
+    return np.sum(getAttr(x, 'vegetables')) - usda['vegetables']
+def usda_proteins(x):
+    return np.sum(getAttr(x, 'proteins')) - usda['proteins']
+def usda_grains(x):
+    return np.sum(getAttr(x, 'grains')) - usda['grains']
+
+result = scipy.optimize.minimize(fun = calories,
+                                 # x0 = [1.0/len(dishes)]*len(dishes),
+                                 x0 = [random.random() * maxRepeats for i in xrange(len(dishes))],
+                                 method='SLSQP',
+                                 bounds = [(0, maxRepeats)]*len(dishes),
+                                 constraints = [
+                                     {'type': 'ineq', 'fun': price},
+                                     {'type': 'ineq', 'fun': usda_fruits},
+                                     {'type': 'ineq', 'fun': usda_vegetables},
+                                     {'type': 'ineq', 'fun': usda_proteins},
+                                     {'type': 'ineq', 'fun': usda_grains},
+                                 ],
+                                 options = {
+                                     'disp': True,
+                                 })
+
+output = np.sum(scale(result.x), axis=0)
+print
+print 'Output'
+for i in xrange(len(output)):
+    print columns[i], ':', output[i]
+
+print
+print 'Number of each dish:'
+for i in xrange(len(result.x)):
+    print dishes[i][0], np.round(result.x[i], 1)
+
+def weightedP(dishValues):
+    if np.sum(dishValues[:4]) == 0:
+        return 1
+    else:
+        return np.sum(np.multiply(p, dishValues[:4])) / np.sum(dishValues[:4])
+
+def actualCalories(x):
+    return np.sum([dishesValues[i][int(columnsLookup['calories'])] * weightedP(dishesValues[i]) * x[i] for i in xrange(len(dishesValues))])
+
+print
+actual_calories = actualCalories(result.x)
+print 'Actual calorie consumption:', actual_calories
+print '=', actual_calories/5.0, 'per day'
