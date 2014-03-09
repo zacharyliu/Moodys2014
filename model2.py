b04af5c51ec6f059da7549d0d2b297c341f02ccd
Zachary Liu <liu.zachary@gmail.com>
2014-03-09 16:05:17 -0400

    sodium

--- model2.py
+++ model2.py
@@ -5,18 +5,42 @@ import scipy.optimize
 import csv
 
 class Model2:
-    params = {
+    params_common = {
         'maxRepeats': 2,
         'P_A': 0.0445,
         'P_B': 0.00513,
         'P_C': -0.018,
+        'price': 7000
+    }
+
+    params_K_5 = {
         'USDA_fruits': 2.5,
         'USDA_vegetables': 3.75,
         'USDA_proteins': 8,
         'USDA_grains': 8,
         'USDA_dairy': 5,
-        'price': 7
+        'USDA_sodium': 640*5,
     }
+
+    params_6_8 = {
+        'USDA_fruits': 2.5,
+        'USDA_vegetables': 3.75,
+        'USDA_proteins': 9,
+        'USDA_grains': 8,
+        'USDA_dairy': 5,
+        'USDA_sodium': 710*5,
+    }
+
+    params_9_12 = {
+        'USDA_fruits': 5,
+        'USDA_vegetables': 5,
+        'USDA_proteins': 10,
+        'USDA_grains': 10,
+        'USDA_dairy': 5,
+        'USDA_sodium': 740*5,
+    }
+
+    params = dict(params_9_12.items() + params_common.items())
 
     def __init__(self, params):
         newParams = {}
@@ -28,7 +52,7 @@ class Model2:
 
         self.maxRepeats = newParams['maxRepeats']
         self.P_A, self.P_B, self.P_C = newParams['P_A'], newParams['P_B'], newParams['P_C']
-        self.usda = {'fruits': newParams['USDA_fruits'], 'vegetables': newParams['USDA_vegetables'], 'proteins': newParams['USDA_proteins'], 'grains': newParams['USDA_grains'], 'dairy': newParams['USDA_dairy']}
+        self.usda = {'fruits': newParams['USDA_fruits'], 'vegetables': newParams['USDA_vegetables'], 'proteins': newParams['USDA_proteins'], 'grains': newParams['USDA_grains'], 'dairy': newParams['USDA_dairy'], 'sodium': newParams['USDA_sodium']}
         self.price = newParams['price']
 
         with open('realPrices2.csv', 'rb') as csvfile:
@@ -54,7 +78,7 @@ class Model2:
     def solve(self):
         result = scipy.optimize.minimize(fun = lambda x: -self.actualCalories(x),
                                          # x0 = [1.0/len(dishes)]*len(dishes),
-                                         x0 = [0 for i in xrange(len(self.dishes))],
+                                         x0 = [self.maxRepeats/2 for i in xrange(len(self.dishes))],
                                          # args = ['calories', 0, True],
                                          method='SLSQP',
                                          bounds = [(0, self.maxRepeats)]*len(self.dishes),
@@ -65,6 +89,7 @@ class Model2:
                                              {'type': 'ineq', 'fun': self.constrainAttr, 'args': ['proteins', self.usda['proteins'], False]},
                                              {'type': 'ineq', 'fun': self.constrainAttr, 'args': ['grains', self.usda['grains'], False]},
                                              {'type': 'ineq', 'fun': self.constrainAttr, 'args': ['dairy', self.usda['dairy'], False]},
+                                             {'type': 'ineq', 'fun': self.constrainAttr, 'args': ['sodium', self.usda['sodium'], True]},
                                          ],
                                          options = {
                                              'disp': True,
@@ -88,12 +113,21 @@ class Model2:
 
         return actual_calories/5.0
 
+    def weightedP(self, dishValues):
+        p = [0.569, 0.403, 0.736, 0.736, 0.539]
+
+        if np.sum(dishValues[:5]) == 0:
+            return 1
+        else:
+            return np.sum(np.multiply(p, dishValues[:5])) / np.sum(dishValues[:5])
+
     def modeledP(self, dishValues):
         sugar = dishValues[int(self.columnsLookup['sugar'])]
         sodium = dishValues[int(self.columnsLookup['sodium'])]
         calcium = dishValues[int(self.columnsLookup['calcium'])]
 
         P = 1-math.e**-(self.P_A*sugar + self.P_B*sodium + self.P_C*calcium)
+        if P<0: print sugar, sodium, calcium, P
         return P
 
     def actualCalories(self, x):
